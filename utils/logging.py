563807bd6581697894d423170fcaf251d3d27318
MeowIce <meowice@meowsmp.net>
2026-09-13 13:27:47 +0700

    refactor: move BIBC configuration to environment

---
+++ utils/logging.py
@@ -0,0 +1,20 @@
+import logging
+import sys
+import time
+
+def configureLogging(logLevel=logging.INFO):
+    formatter = logging.Formatter(
+        fmt="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    formatter.converter = time.gmtime
+    
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(formatter)
+    
+    rootLogger = logging.getLogger()
+    rootLogger.setLevel(logLevel)
+    
+    rootLogger.handlers.clear()
+    rootLogger.addHandler(handler)
+    return rootLogger
